ef4d9e5260551a2efe200e5677c7e5845b93e709
Cristiano <cristiano@cesaretto.it>
2024-03-18 17:20:02 +0000

    added account store loader

---
+++ Melior.InterviewQuestion/Data/AccountDataLoader.cs
@@ -0,0 +1,26 @@
+﻿using Melior.InterviewQuestion.Types;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melior.InterviewQuestion.Data
+{
+    public class AccountDataLoader : IAccountDataLoader
+    {
+        public IAccountDataStore GetCurrentAccountDataStore()
+        {
+            var dataStoreType = ConfigurationManager.AppSettings["DataStoreType"];
+            if (dataStoreType == "Backup")
+            { 
+                return new BackupAccountDataStore(); 
+            }
+            else
+            {
+                return new AccountDataStore();
+            }
+        }
+    }
+}
