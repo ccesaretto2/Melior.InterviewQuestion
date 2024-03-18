b66cd1e60ae32157d4fba5c31dd579d4a72b96f6
Cristiano <cristiano@cesaretto.it>
2024-03-18 17:19:34 +0000

    added interface for account data store

---
+++ Melior.InterviewQuestion/Data/IAccountDataStore.cs
@@ -0,0 +1,15 @@
+﻿using Melior.InterviewQuestion.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melior.InterviewQuestion.Data
+{
+    public interface IAccountDataStore
+    {
+        Account GetAccount(string accountNumber);
+        void UpdateAccount(Account account);
+    }
+}
